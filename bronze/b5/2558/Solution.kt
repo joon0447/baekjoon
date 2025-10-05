da743f9a8868bdac8726e9da1f41baa6b5f3db51
조상준 <94978051+joon0447@users.noreply.github.com>
2025-10-05 01:55:13 +0000

    b5 5문제

---
+++ bronze/b5/2558/Solution.kt
@@ -0,0 +1,10 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val a = br.readLine().toInt()
+    val b = br.readLine().toInt()
+
+    println(a+b)
+}
---
+++ bronze/b5/2742/Solution.kt
@@ -0,0 +1,11 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+
+    for(i in n downTo 1){
+        println(i)
+    }
+}
