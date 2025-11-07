01e92d7ca6fd65d282d31523b3a675d1657b5dd4
Sangjun Jo <94978051+joon0447@users.noreply.github.com>
2025-11-07 09:36:01 +0900

    [Bronze V] Title: 개, Time: 68 ms, Memory: 11820 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.kt
@@ -0,0 +1,13 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val answer = StringBuilder()
+    answer.append("|\\_/|\n")
+    answer.append("|q p|   /}\n")
+    answer.append("( 0 )\"\"\"\\\n")
+    answer.append("|\"^\"`    |\n")
+    answer.append("||_/=\\\\__|\n")
+    println(answer.toString())
+}
