e2a27ce185f6fd030de860a852edc0444f756683
Sangjun Jo <94978051+joon0447@users.noreply.github.com>
2025-11-14 12:04:19 +0900

    [Bronze V] Title: X보다 작은 수, Time: 184 ms, Memory: 23164 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.kt
@@ -0,0 +1,11 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val (n, x) = br.readLine().split(" ").map { it.toInt() }
+    val list = br.readLine().split(" ").map { it.toInt() }
+
+    val answer = list.filter { it < x }
+    println(answer.joinToString(" "))
+}
