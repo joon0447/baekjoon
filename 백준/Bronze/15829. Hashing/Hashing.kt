f8f963afd255bac55d9da272bf30c946cbf2426b
Sangjun Jo <94978051+joon0447@users.noreply.github.com>
2025-11-17 13:14:13 +0900

    [Bronze II] Title: Hashing, Time: 76 ms, Memory: 12192 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.kt
+++ 백준/Bronze/15829. Hashing/Hashing.kt
@@ -1,18 +1,21 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import kotlin.math.pow
 
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val n = br.readLine().toInt()
     val str = br.readLine()
 
-    var count = 0
+    val m = 1234567891L
+
     var answer = 0L
-    for(c in str) {
-        val s = c.code - 96
-        answer += (s * 31.0.pow(count)).toLong()
-        count++
+    var rPow = 1L
+
+    for (c in str) {
+        val s = (c.code - 96).toLong()
+
+        answer = (answer + s * rPow) % m
+        rPow = (rPow * 31L) % m
     }
 
     println(answer)
